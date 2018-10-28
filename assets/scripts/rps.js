1cc4469380e69cf9f6d6bbcf520f14c2658bdc90
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-10-28 16:06:31 -0400

    initial commit of files

---
+++ assets/scripts/rps.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyBJigd9ReUwomdk4C5unw9gPiS8CYT5V44",
+    authDomain: "rps-mp-d43ec.firebaseapp.com",
+    databaseURL: "https://rps-mp-d43ec.firebaseio.com",
+    projectId: "rps-mp-d43ec",
+    storageBucket: "rps-mp-d43ec.appspot.com",
+    messagingSenderId: "499162764941"
+  };
+  firebase.initializeApp(config);
