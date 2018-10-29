55a381b785ffa579028fc9ac8bf8c7ddbcc6dc54
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-10-29 07:29:27 -0400

    initial submit player logic to firebase

--- assets/scripts/rps.js
+++ assets/scripts/rps.js
@@ -7,3 +7,51 @@ const config = {
     messagingSenderId: "499162764941"
   };
   firebase.initializeApp(config);
+  database = firebase.database();
+
+  //initialize player objects as null (not assigned yet)
+  let player1 = null;
+  let player2 = null;
+
+  let localPlayerName;
+
+  let playerNameInput = $(`#player-name-input`);
+
+  $(document).ready(function(){
+
+    //event listener on player submit button to add player to database
+    $(`#submit-player-button`).click(function(event){
+        console.log(`clicked`);
+        //check if name is not blank and if game is waiting for 2 assigned players
+        if(playerNameInput !== "" && !(player1 && player2)){
+            //check if player1 null, then check player2
+            if(player1 === null){
+                player1 = {
+                    name: playerNameInput.val(),
+                    wins:0,
+                    losses:0,
+                    currentChoice: ""
+                }
+
+                database.ref().child('/players/player1').set(player1);
+
+            } else if(player2 === null){
+                player2 = {
+                    name: playerNameInput.val().trim(),
+                    wins:0,
+                    losses:0,currentChoice: ""
+                }
+
+                database.ref().child('/players/player2').set(player2);
+
+            }
+
+
+        } else{
+            console.log(`name blank or all players assigned`);
+        }
+    })
+
+
+
+  });
